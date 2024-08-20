19b6333d9710830d2e8d555f1887bd73a8f8bbe8
Daniel Parsons <dpars@hotmail.co.uk>
2024-08-20 23:46:49 +0900

    changed package for swipe cards

--- lib/screens/deck/swipe_screen.dart
+++ lib/screens/deck/swipe_screen.dart
@@ -6,9 +6,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flashy_flutter/models/cards_data.dart';
 import 'package:flashy_flutter/widgets/swipe_card.dart';
-import 'package:swipe_cards/swipe_cards.dart';
 import '../../notifiers/deck_notifier.dart';
-import '../../widgets/swipe_tag.dart';
+
+import 'package:appinio_swiper/appinio_swiper.dart';
+
 
 class SwipeScreen extends ConsumerStatefulWidget {
   const SwipeScreen({Key? key, required this.title, required this.id}) : super(key: key);
@@ -21,7 +22,7 @@ class SwipeScreen extends ConsumerStatefulWidget {
 }
 
 class _SwipeScreenState extends ConsumerState<SwipeScreen> {
-  late MatchEngine _matchEngine;
+  final AppinioSwiperController controller = AppinioSwiperController();
   bool _isLoading = true;
   int _swipeCounter = 1;
   int _totalCount = 0;
@@ -34,40 +35,66 @@ class _SwipeScreenState extends ConsumerState<SwipeScreen> {
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      _initializeMatchEngine();
+        _isLoading = false;
       _startTimer();
     });
   }
 
-  void _initializeMatchEngine() {
-    final deckDataList = ref.read(deckProvider);
+  // void _initializeMatchEngine() {
+  //   final deckDataList = ref.read(deckProvider);
+  //
+  //   // Ensure the deck exists
+  //   final deck = deckDataList.firstWhere((deck) => deck.id == widget.id, orElse: () {
+  //     throw Exception('Deck with id ${widget.id} not found');
+  //   });
+  //
+  //   final List<CardsData>? allCards = deck.cards;
+  //   _totalCount = allCards?.length ?? 0;
+  //
+  //   final swipeItems = allCards?.map((item) => SwipeItem(
+  //     content: SwipeCard(item: item),
+  //     likeAction: () {
+  //       setState(() {
+  //         _swipeCounter++;
+  //       });
+  //     },
+  //     nopeAction: () {
+  //       setState(() {
+  //         _swipeCounter++;
+  //       });
+  //     },
+  //   )).toList();
+  //
+  //   setState(() {
+  //     _matchEngine = MatchEngine(swipeItems: swipeItems);
+  //     _isLoading = false;
+  //   });
+  // }
 
-    // Ensure the deck exists
-    final deck = deckDataList.firstWhere((deck) => deck.id == widget.id, orElse: () {
-      throw Exception('Deck with id ${widget.id} not found');
-    });
+  void _swipeEnd(int previousIndex, int targetIndex, SwiperActivity activity) {
+    switch (activity) {
+      case Swipe():
+        print('The card was swiped to the : ${activity.direction}');
+        print('previous index: $previousIndex, target index: $targetIndex');
+        break;
+      case Unswipe():
+        print('A ${activity.direction.name} swipe was undone.');
+        print('previous index: $previousIndex, target index: $targetIndex');
+        break;
+      case CancelSwipe():
+        print('A swipe was cancelled');
+        break;
+      case DrivenActivity():
+        print('Driven Activity');
+        break;
+    }
+  }
 
-    final List<CardsData>? allCards = deck.cards;
-    _totalCount = allCards?.length ?? 0;
-
-    final swipeItems = allCards?.map((item) => SwipeItem(
-      content: SwipeCard(item: item),
-      likeAction: () {
-        setState(() {
-          _swipeCounter++;
-        });
-      },
-      nopeAction: () {
-        setState(() {
-          _swipeCounter++;
-        });
-      },
-    )).toList();
-
-    setState(() {
-      _matchEngine = MatchEngine(swipeItems: swipeItems);
-      _isLoading = false;
-    });
+  void _onEnd () {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => ResultsScreen(id: 1,)),
+    );
   }
 
   void _startTimer() {
@@ -134,23 +161,27 @@ class _SwipeScreenState extends ConsumerState<SwipeScreen> {
               ],
             ),
             const SizedBox(height: 24),
-            Flexible(
-              child: SwipeCards(
-                matchEngine: _matchEngine,
-                itemBuilder: (BuildContext context, int index) {
-                  return SwipeCard(item: deck.cards![index]);
-                },
-                onStackFinished: () {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => ResultsScreen(id: 1,)),
-                  );
-                },
-                upSwipeAllowed: false,
-                fillSpace: false,
-                likeTag: SwipeTag(color: 'green', text: deck.rightOption,),
-                nopeTag: SwipeTag(color: 'red', text: deck.leftOption,),
-              ),
+            LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+                double width = constraints.maxWidth;
+                return SizedBox(
+                  width: width,
+                  height: width,
+                  child: AppinioSwiper(
+                    invertAngleOnBottomDrag: true,
+                    backgroundCardCount: 0,
+                    swipeOptions: const SwipeOptions.all(),
+                    controller: controller,
+                    onSwipeEnd: _swipeEnd,
+                    onEnd: _onEnd,
+                    cardCount: deck.cards!.length,
+                    threshold: 25,
+                    cardBuilder: (BuildContext context, int index) {
+                      return SwipeCard(item: deck.cards![index]);
+                    },
+                  ),
+                );
+              },
             ),
             const SizedBox(height: 24),
             Row(
